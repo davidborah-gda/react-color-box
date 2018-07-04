f12b42dab74a63fd2c5b82dd22302c3c8535ca2d
David Borah <dborah87@gmail.com>
2018-07-03 21:02:11 -0400

    was able to get the box text to change but not the backgorund color yet

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      color: "red"
+    };
+  }
+
+  red(){
+    this.setState({
+      color: "red"
+    });
+  }
+  green(){
+    this.setState({
+      color: "green"
+    });
+  }
   render() {
+    const styles = {backgroundColor: 'red'};
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <button onClick={this.red.bind(this)}>Red</button>
+        <h1 className = "box" style = {styles}>{this.state.color}</h1>
+        <button onClick={this.green.bind(this)}>Green</button>
       </div>
     );
   }
