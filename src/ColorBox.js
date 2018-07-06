3f74a67f42bcd1593995042fcc6da68d03f2bb2b
David Borah <dborah87@gmail.com>
2018-07-06 18:47:03 -0400

    Fixed the Color inputs to reflect the default colors and added functionality for each selector input and both buttons

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -7,37 +7,49 @@ class ColorBox extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      color: "red"
+      color: "green",
+      first: "red",
+      second: "yellow"
     };
   }
 
   First() {
     this.setState({
-      color: this.state.color
+      color: this.state.first
     });
   }
   Second() {
     this.setState({
-      color: this.state.color
+      color: this.state.second
     });
   }
 
   changeColor(evt) {
 
     this.setState({
-      color: (evt.target.value)
+      color: "red"
     });
   }
+
+  changeFirstColor(evt){
+    this.setState({
+     first: evt.target.value
+    })
+  }
+
+  changeSecondColor(evt){
+    this.setState({
+      second: evt.target.value
+    })
+  }
+  
 
   render() {
     const styles = { backgroundColor: this.state.color };
     return (
       <div className="ColorBox-container">
         {this.props.title ? <h1 className="ColorBox-title">{this.props.title}</h1> : null}
-
         <h1 className="box" style={styles}></h1>
-
-
         <section>
           <button onClick={this.First.bind(this)} className="ColorBox-button">First</button>
           <button onClick={this.Second.bind(this)} className="ColorBox-button">Second</button>
@@ -45,15 +57,15 @@ class ColorBox extends Component {
         <section><h2>First Color</h2>
           <input
             placeholder="HEX Color Code"
-
-            onChange={this.changeColor.bind(this)}
-            className="ColorBox-input" />
+            onChange={this.changeFirstColor.bind(this)}
+            className="ColorBox-input"
+            value= {this.state.first} />
           <h2>Second Color</h2>
           <input
             placeholder="HEX Color Code"
-
-            onChange={this.changeColor.bind(this)}
-            className="ColorBox-input" />
+            onChange={this.changeSecondColor.bind(this)}
+            className="ColorBox-input"
+            value= {this.state.second} />
         </section>
       </div>
     )
